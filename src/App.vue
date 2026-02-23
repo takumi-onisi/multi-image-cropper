b3f10f8266ac01b062db7df68ee5d9f037d134d1
t93onsi <t93onisi@gmail.com>
2026-02-23 22:09:01 +0900

    アップデート: 画像データを取得できるかテスト用の実装

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
-<script setup>import { useImagesStore } from './stores/imagesStore'
-const imagesStore = useImagesStore()
-console.log('store initialized : '+imagesStore.fileList)
-
+<script setup>
+import { useImagesStore } from "./stores/imagesStore";
+import ImageDropZone from "./components/ImageDropZone.vue";
+const imagesStore = useImagesStore();
 </script>
 
 <template>
   <h1>Image Crop Tool</h1>
   <p>現在のファイル数 : {{ imagesStore.fileList.length }}</p>
+
+  <div>
+    <h2>画像切り抜き</h2>
+    <ImageDropZone />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
