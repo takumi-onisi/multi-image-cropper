bb45646720af50a2c441facaaef286e2b2993dc9
t93onsi <t93onisi@gmail.com>
2026-02-23 22:08:08 +0900

    追加: 画像をドラッグアンドドロップで取得するエリアのコンポーネントを作成

---
+++ src/components/ImageDropZone.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useImagesStore } from '../stores/imagesStore';
+const store = useImagesStore();
+
+const handleDrop = (e) => {
+  const files = e.dataTransfer.files;
+  if (files.length > 0) {
+    // ストアの関数のテスト
+    store.addFiles(files);
+  }
+};
+</script>
+
+<template>
+  <div 
+    @dragover.prevent 
+    @drop.prevent="handleDrop"
+    style="border: 2px dashed #000; padding: 50px; text-align: center;"
+  >
+    ここに画像をドロップしてください
+  </div>
+</template>
