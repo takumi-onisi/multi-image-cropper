81ef446cfe9bc1d1d9bd7bb9ff8b9bd9a7bd00d4
t93onsi <t93onisi@gmail.com>
2026-02-27 20:26:37 +0900

    アップデート: templateで実現したい機能に沿ったdomを渡しておく。(propper.jのver.2ではこのやり方が主流？)

--- src/components/CropperPanel.vue
+++ src/components/CropperPanel.vue
@@ -14,11 +14,37 @@ const initCropper = () => {
   if (cropper) cropper.destroy();
   if (!imageElement.value) return;
 
-  cropper = new Cropper(imageElement.value, {
-    viewMode: 1,
-    movable: true,
-    zoomable: true,
-  });
+  const template = `
+  <cropper-canvas background>
+    <cropper-image translatable scalable action="move"></cropper-image>
+    
+    <cropper-shade></cropper-shade>
+
+    <cropper-selection initial-coverage="0.5" movable resizable>
+      <cropper-grid role="grid" bordered covered></cropper-grid>
+      <cropper-crosshair centered></cropper-crosshair>
+      
+      <cropper-handle action="move" theme-color="rgba(255, 255, 255, 0.35)"></cropper-handle>
+      
+      <cropper-handle action="n-resize"></cropper-handle>
+      <cropper-handle action="e-resize"></cropper-handle>
+      <cropper-handle action="s-resize"></cropper-handle>
+      <cropper-handle action="w-resize"></cropper-handle>
+      <cropper-handle action="ne-resize"></cropper-handle>
+      <cropper-handle action="nw-resize"></cropper-handle>
+      <cropper-handle action="se-resize"></cropper-handle>
+      <cropper-handle action="sw-resize"></cropper-handle>
+    </cropper-selection>
+  </cropper-canvas>
+  `;
+
+  cropper = new Cropper(imageElement.value, { template });
+};
+
+const saveConfig = () => {
+  if (!cropper) return;
+
+  const data = cropper.getData();
 };
 
 // 一枚目の画像が読み込まれたら初期化
