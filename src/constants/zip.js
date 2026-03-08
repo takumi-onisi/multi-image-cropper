d4837709c0b6f095b49f7e1b3fdce0659d5a8550
t93onsi <t93onisi@gmail.com>
2026-03-08 19:49:35 +0900

    アップデート: 切り抜いた画像をZIPにまとめてダウンロードするロジックをutilsに移す

---
+++ src/constants/zip.js
@@ -0,0 +1,1 @@
+export const DEFAULT_ZIP_FILENAME = "cropped_images.zip";
---
+++ src/utils/zip.js
@@ -0,0 +1,27 @@
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
+import { DEFAULT_ZIP_FILENAME } from '../constants/zip';
+
+/**
+ * @typedef {Object} ZipFile
+ * @property {string} name - ファイル名
+ * @property {Blob} blob - ファイルのバイナリデータ
+ */
+
+/**
+ * 複数のファイルをZIPにまとめてダウンロードする
+ * * @param {ZipFile[]} files - ZIPに含めるファイル情報の配列
+ * @param {string} [zipName="download.zip"] - 生成するZIPファイル名
+ */
+export const downloadFilesAsZip = async (files, zipName = DEFAULT_ZIP_FILENAME) => {
+  if (files.length === 0) return;
+
+  const zip = new JSZip();
+  
+  files.forEach(({ name, blob }) => {
+    zip.file(name, blob);
+  });
+
+  const zipContent = await zip.generateAsync({ type: "blob" });
+  saveAs(zipContent, zipName);
+};
