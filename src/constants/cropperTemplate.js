481a59ec7de64fda18aeaaca02a3a8af85404ed1
t93onsi <t93onisi@gmail.com>
2026-03-24 08:28:47 +0900

    修正: 今後の方針としてクロッパーはおおよその切り抜き位置を示すUIとしての利用にとどめるのでpreciseは削除する

--- src/constants/cropperTemplate.js
+++ src/constants/cropperTemplate.js
@@ -3,7 +3,7 @@ export const CROPPER_TEMPLATE = `
     <cropper-image translatable scalable action="move"></cropper-image>
     <cropper-handle action="move" theme-color="rgba(255, 255, 255, 0.35)"></cropper-handle>
     <cropper-shade></cropper-shade>
-    <cropper-selection initial-coverage="0.5" movable resizable precise>
+    <cropper-selection initial-coverage="0.5" movable resizable>
       <cropper-grid role="grid" bordered covered></cropper-grid>
       <cropper-crosshair centered></cropper-crosshair>
       <cropper-handle action="move" theme-color="rgba(255, 255, 255, 0.35)"></cropper-handle>
