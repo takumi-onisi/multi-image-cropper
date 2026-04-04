c8c289c553384c39cccc06f47d85830e9c465067
t93onsi <t93onisi@gmail.com>
2026-04-04 21:35:47 +0900

    アップデート: 切抜きモードと切り抜きサイズを追加したcropConfigのデフォルト値を定義

--- src/constants/cropConfig.js
+++ src/constants/cropConfig.js
@@ -1,1 +1,10 @@
+import { CROP_MODES } from "./cropModes";
+
 export const GLOBAL_PREVIEW_ID = "GLOBAL_DEFAULT_PREVIEW_MASTER";
+
+export const DEFAULT_CROP_CONFIG = {
+  mode: CROP_MODES.FREE,
+  targetSize: { width: 0, height: 0 },
+  selection: { x: 0, y: 0, width: 0, height: 0 },
+  transform: [1, 0, 0, 1, 0, 0],
+};
