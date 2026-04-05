696d6fcbc89666fcceb9f88ce3b89652c63d9cde
t93onsi <t93onisi@gmail.com>
2026-04-05 15:43:49 +0900

    アップデート: cropConfigのプロパティにaspectRatioを追加

--- src/constants/cropConfig.js
+++ src/constants/cropConfig.js
@@ -4,6 +4,7 @@ export const GLOBAL_PREVIEW_ID = "GLOBAL_DEFAULT_PREVIEW_MASTER";
 
 export const DEFAULT_CROP_CONFIG = {
   mode: CROP_MODES.FREE,
+  aspectRatio: 1,
   targetSize: { width: 0, height: 0 },
   selection: { x: 0, y: 0, width: 0, height: 0 },
   transform: [1, 0, 0, 1, 0, 0],
