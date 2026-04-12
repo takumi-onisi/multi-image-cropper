2d5e69cf4e27f05d96e1df8b155ca5ace43a7891
t93onsi <t93onisi@gmail.com>
2026-04-12 19:37:18 +0900

    アップデート: 画像生成処理での拡張子の取り扱いを定数を使用した形に変更

--- src/constants/exportTypes.js
+++ src/constants/exportTypes.js
@@ -8,3 +8,17 @@ export const EXPORT_TYPE_OPTIONS = [
   { label: "PNG (.png)", value: EXPORT_TYPES.PNG },
   { label: "JPEG (.jpg)", value: EXPORT_TYPES.JPEG },
 ];
+
+const EXTENSIONS = {
+  [EXPORT_TYPES.PNG]: "png",
+  [EXPORT_TYPES.JPEG]: "jpg",
+};
+
+/**
+ * MIMEタイプに対応する拡張子を取得する
+ * @param {string} type
+ * @returns {string} 拡張子（デフォルトはpng）
+ */
+export const getExtensionByType = (type) => {
+  return EXTENSIONS[type] || "png";
+};
