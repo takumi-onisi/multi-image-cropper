49b5f6fa4d4a38a7fae7ba433cb18a31a73ea1bb
t93onsi <t93onisi@gmail.com>
2026-04-21 22:38:26 +0900

    アップデート: 初回起動時のチュートリアル用の画像オブジェクトを用意

---
+++ src/constants/tutorialImages.js
@@ -0,0 +1,31 @@
+// 画像をインポート
+import turorialImage1 from "../assets/images/tutorial/image-01.jpg";
+import turorialImage2 from "../assets/images/tutorial/image-02.jpg";
+import turorialImage3 from "../assets/images/tutorial/image-03.jpg";
+import turorialImage4 from "../assets/images/tutorial/image-04.jpg";
+
+/**
+ * チュートリアル用のダミー画像データ
+ */
+export const TUTORIAL_IMAGES = [
+  {
+    id: "tutorial-1",
+    name: "image-1.jpg",
+    url: turorialImage1,
+  },
+  {
+    id: "tutorial-2",
+    name: "image-2.jpg",
+    url: turorialImage2,
+  },
+  {
+    id: "tutorial-3",
+    name: "image-3.jpg",
+    url: turorialImage3,
+  },
+  {
+    id: "tutorial-4",
+    name: "image-4.jpg",
+    url: turorialImage4,
+  },
+];
