95673a60510916f5eb23af4bc167200916b8f752
t93onsi <t93onisi@gmail.com>
2026-04-22 20:28:11 +0900

    アップデート: アプリにフッターを設置

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,40 @@ import ImageDropZone from "./components/ImageDropZone.vue";
 import GlobalSettingView from "./components/GlobalSettingView.vue";
 import CropPreviewGrid from "./components/CropPreviewGrid.vue";
 import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
 import "./assets/css/main.css";
 const imagesStore = useImagesStore();
 imagesStore.initTutorial();
 </script>
 
 <template>
-  <AppHeader />
+  <div class="app-wrapper">
+    <AppHeader />
 
-  <div class="container">
-    <ImageDropZone />
-    <GlobalSettingView />
-  </div>
+    <main class="content">
+      <div class="container">
+        <ImageDropZone />
+        <GlobalSettingView />
+      </div>
 
-  <div class="container">
-    <CropPreviewGrid />
+      <div class="container">
+        <CropPreviewGrid />
+      </div>
+    </main>
+
+    <AppFooter />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app-wrapper {
+  display: flex;
+  flex-direction: column;
+  /* 最低でも画面いっぱいの高さを確保する */
+  min-height: 100vh;
+}
+
+.content {
+  flex: 1;
+}
+</style>
